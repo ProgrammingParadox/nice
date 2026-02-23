58c63a75e25dd08f91fadf0a738ecfb82f5968df
programmingparadox <theparadox31415@gmail.com>
2026-02-23 14:04:56 -0800

    I don't even know anymore. Routine checkup.

---
+++ scenes/character_body_2d.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
+
+var time_since_on_ground = -1;
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+		time_since_on_ground += delta;
+	
+	if (is_on_floor() or 
+		$wall_jump_rays/right_wall_ray.is_colliding() or 
+		$wall_jump_rays/left_wall_ray.is_colliding()):
+		time_since_on_ground = 0;
+
+	# Handle jump.
+	if Input.is_action_pressed("up") and time_since_on_ground < 0.1:
+		velocity.y = JUMP_VELOCITY
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
