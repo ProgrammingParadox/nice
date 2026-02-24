11b3179f1376a7ad7de431cc1ad1301fd345aa11
programmingparadox <theparadox31415@gmail.com>
2026-02-24 14:06:15 -0800

    friction against wall only when falling and pressed against it; jump buffer

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -2,13 +2,22 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
+const WALL_FRICTION = 0.8
 
 var time_since_on_ground = -1
 var time_since_last_wall_touch = -1
 var time_since_last_wall_jump = -1
 
+var time_since_jump_pressed = INF
+
 
 func _physics_process(delta: float) -> void:
+	time_since_jump_pressed += delta
+	if Input.is_action_pressed("up"):
+		time_since_jump_pressed = 0
+
+	var jump = time_since_jump_pressed < 0.1
+
 	# Add the gravity.
 	if is_on_floor():
 		time_since_on_ground = 0
@@ -32,19 +41,19 @@ func _physics_process(delta: float) -> void:
 	):
 		time_since_last_wall_touch = 0
 
-		if Input.get_axis("left", "right") != 0:
-			velocity.y *= 0.8
+		if Input.get_axis("left", "right") != 0 and velocity.y > 0.0:
+			velocity.y *= WALL_FRICTION
 	else:
 		time_since_last_wall_touch += delta
 
 	#print(time_since_last_wall_touch)
 
 	# Handle jump.
-	if Input.is_action_pressed("up") and time_since_on_ground < 0.1:
+	if jump and time_since_on_ground < 0.1:
 		velocity.y = JUMP_VELOCITY
 
 	time_since_last_wall_jump += delta
-	if Input.is_action_pressed("up") and time_since_last_wall_touch < 0.1:
+	if jump and time_since_last_wall_touch < 0.1:
 		if left_wall_ray_collided and Input.is_action_pressed("right"):
 			velocity.x = -JUMP_VELOCITY * 0.2
 			velocity.y = JUMP_VELOCITY
@@ -52,7 +61,7 @@ func _physics_process(delta: float) -> void:
 			velocity.x = JUMP_VELOCITY * 0.2
 			velocity.y = JUMP_VELOCITY
 
-		time_since_last_wall_jump = 1000
+		time_since_last_wall_jump = INF
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
