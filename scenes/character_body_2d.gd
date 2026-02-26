73adbd4ff3265855add0d4222e65961c1aa1e119
programmingparadox <theparadox31415@gmail.com>
2026-02-26 14:47:11 -0800

    Improved wall jump and general movement; switched player X to vel based

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
+const SPEED = 200.0 # actually acceleration...
+const MAX_SPEED = 200.0
 const JUMP_VELOCITY = -300.0
+const WALL_JUMP_VELOCITY = JUMP_VELOCITY * 0.7
 const MAX_WALL_CLING_SPEED = 100
+const AIRTIME_X_VEL_MOD = 0.9
 
 var time_since_on_ground = INF
 var time_since_last_wall_touch = INF
@@ -13,8 +16,23 @@ var time_since_jump_pressed = INF
 var gravity_mod := 1.0
 var velocity_c_x = 0.0
 
+var dead_actions = []
+
 
 func _physics_process(delta: float) -> void:
+	# because array.erase doesn't support
+	# the functionality while looping
+	# over the array, we'll buffer actions
+	# that need to be removed. I don't like
+	# it, but whatever.
+	var to_remove = []
+	for action in dead_actions:
+		if not Input.is_action_pressed(action):
+			to_remove.append(action)
+
+	for action in to_remove:
+		dead_actions.erase(action)
+
 	# keep track of jumping
 	time_since_jump_pressed += delta
 	if Input.is_action_pressed("up"):
@@ -75,37 +93,48 @@ func _physics_process(delta: float) -> void:
 	time_since_last_wall_jump += delta
 	if (
 		jump and
+		not is_on_floor() and
 		time_since_last_wall_touch < 0.1 and
 		time_since_last_wall_jump > 0.1
 	):
 		if (
 			left_wall_area_collided and
 			not right_wall_area_collided and
-			Input.is_action_pressed("right")
+			Input.is_action_pressed("right") and
+			not "right" in dead_actions
 		):
-			velocity.x = -JUMP_VELOCITY * 1
+			velocity.x = -WALL_JUMP_VELOCITY
 			velocity.y = JUMP_VELOCITY
 
 			time_since_last_wall_jump = 0
+
+			dead_actions.append("right")
 		if (
 			right_wall_area_collided and
 			not left_wall_area_collided and
-			Input.is_action_pressed("left")
+			Input.is_action_pressed("left") and
+			not "left" in dead_actions
 		):
-			velocity.x = JUMP_VELOCITY * 1
+			velocity.x = WALL_JUMP_VELOCITY
 			velocity.y = JUMP_VELOCITY
 
 			time_since_last_wall_jump = 0
 
+			dead_actions.append("left")
+
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		#velocity.x = direction * SPEED
-		velocity.x = (SPEED * direction)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		var vel = (SPEED * direction * delta * 20)
+		if not is_on_floor():
+			vel *= AIRTIME_X_VEL_MOD
 
-	#velocity.x *= 0.9
+		velocity.x += vel
+
+	#else:
+	#velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	velocity.x *= 0.8
+	velocity.x = min(MAX_SPEED, velocity.x)
 
 	move_and_slide()
