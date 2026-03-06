ae5310835d69d748ce8b14874638aa884e574726
programmingparadox <theparadox31415@gmail.com>
2026-03-06 14:32:14 -0800

    Camera tracks closer based on distance

--- main.gd
+++ main.gd
@@ -20,4 +20,16 @@ func lerp_v(v0: Vector2, v1: Vector2, t: float) -> Vector2:
 func _process(delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
 
-	$Camera2D.global_position = lerp_v($Camera2D.global_position, $Player.global_position, 0.1)
+	var max_distance = 100
+
+	$Camera2D.global_position = lerp_v(
+		$Camera2D.global_position,
+		$Player.global_position,
+		remap(
+			min($Player.global_position.distance_to($Camera2D.global_position), max_distance),
+			0,
+			max_distance,
+			0,
+			1,
+		),
+	)
