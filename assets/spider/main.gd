720c94184874fd3a5b7e33aa17ddb0575cd7ecc6
programmingparadox <theparadox31415@gmail.com>
2026-04-24 14:26:11 -0700

    Revert "ShapeCast2D instead of RayCast2D for Spider"
    This reverts commit c663aad0de321c51b1d52dfc127dcb635541c0c3.

--- assets/spider/main.gd
+++ assets/spider/main.gd
@@ -1,16 +1,12 @@
 extends Node2D
 
 @onready var rays = [
-	#[$Skeleton2D/rays/raycast_1, $targets/target_1, $transitional_targets/t_target_1],
-	#[$Skeleton2D/rays/raycast_2, $targets/target_2, $transitional_targets/t_target_2],
-	#[$Skeleton2D/rays/raycast_3, $targets/target_3, $transitional_targets/t_target_3],
-	#[$Skeleton2D/rays/raycast_4, $targets/target_4, $transitional_targets/t_target_4],
-	[$Skeleton2D/rays/ShapeCast2D_1, $targets/target_1, $transitional_targets/t_target_1],
-	[$Skeleton2D/rays/ShapeCast2D_2, $targets/target_2, $transitional_targets/t_target_2],
-	[$Skeleton2D/rays/ShapeCast2D_3, $targets/target_3, $transitional_targets/t_target_3],
-	[$Skeleton2D/rays/ShapeCast2D_4, $targets/target_4, $transitional_targets/t_target_4],
-	#[$Skeleton2D/rays/raycast_5, $targets/target_5, $transitional_targets/t_target_5],
-	#[$Skeleton2D/rays/raycast_6, $targets/target_6, $transitional_targets/t_target_6],
+	[$Skeleton2D/rays/raycast_1, $targets/target_1, $transitional_targets/t_target_1],
+	[$Skeleton2D/rays/raycast_2, $targets/target_2, $transitional_targets/t_target_2],
+	[$Skeleton2D/rays/raycast_3, $targets/target_3, $transitional_targets/t_target_3],
+	[$Skeleton2D/rays/raycast_4, $targets/target_4, $transitional_targets/t_target_4],
+	[$Skeleton2D/rays/raycast_5, $targets/target_5, $transitional_targets/t_target_5],
+	[$Skeleton2D/rays/raycast_6, $targets/target_6, $transitional_targets/t_target_6],
 ]
 
 var enemies: Array[Node] = []
@@ -60,7 +56,7 @@ func _process(delta: float) -> void:
 			print("dead")
 
 	for ray_pair in rays:
-		var ray: ShapeCast2D = ray_pair[0]
+		var ray = ray_pair[0]
 		var target = ray_pair[1]
 		var t_target = ray_pair[2]
 
@@ -70,47 +66,8 @@ func _process(delta: float) -> void:
 		if ray == null || target == null:
 			continue
 
-		# shapecasts
-		if not ray.is_colliding():
-			continue
-
-		var sacrifice: RayCast2D = $Skeleton2D/rays/raycast_1
-
-		var closest_distance = INF
-		var closest_point
-		for i in range(ray.get_collision_count()):
-			var collision = ray.get_collision_point(i)
-			var collider = ray.get_collider(i)
-
-			sacrifice.target_position = collision - sacrifice.global_position
-			sacrifice.force_raycast_update()
-
-			if sacrifice.get_collider() != collider:
-				continue
-
-			var collision_point = sacrifice.get_collision_point()
-			var dist = collision_point.distance_to(self.global_position)
-
-			if dist < closest_distance:
-				closest_distance = dist
-				closest_point = collision_point
-
-		if closest_point == null:
-			continue
-
-		var distance = closest_point.distance_to(target.global_position)
-
-		if (
-			(distance > 80) or
-			(target.global_position.distance_to(t_target.global_position) > 80)
-		):
-			print(t_target.global_position)
-			t_target.global_position = closest_point
-
-# for raycasts
-"""
 		ray.rotation = sin(Engine.get_physics_frames()) * 0.3
-		
+
 		if ray.is_colliding():
 			var point = ray.get_collision_point()
 
@@ -122,4 +79,3 @@ func _process(delta: float) -> void:
 				(target.global_position.distance_to(t_target.global_position) > 80)
 			):
 				t_target.global_position = point
-"""
